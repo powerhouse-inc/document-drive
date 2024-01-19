1ffb00443bf442a17e545f2451e9b399edfcc0d3
Frank <frank.pfeift@protonmail.com>
2024-01-19 10:26:30 +0100

    feat: added basic implementation of push and pull transmitter

--- src/transmitters/pull-responder.ts
+++ src/transmitters/pull-responder.ts
@@ -1,5 +1,40 @@
+import { OperationScope } from 'document-model/document';
+import { DocumentDriveServer, StrandUpdate } from '..';
+
 export class PullResponderTransmitter {
-    static process() {
-        // fetch
+    protected drive: DocumentDriveServer;
+    constructor(drive: DocumentDriveServer) {
+        this.drive = drive;
+    }
+
+    async getStrands(
+        listenerId: string,
+        since?: Date
+    ): Promise<StrandUpdate[]> {
+        const cacheEntries = this.drive.getCacheEntries(listenerId);
+        const strands: StrandUpdate[] = [];
+        for (const entry of cacheEntries) {
+            const doc = await this.drive.getDocument(
+                entry.driveId,
+                entry.syncUnit.documentId
+            );
+            const scope = entry.syncUnit.scope as OperationScope;
+            const operations = doc.operations[scope];
+            if (doc.revision[scope] > entry.syncUnit.revision) {
+                strands.push({
+                    driveId: entry.driveId,
+                    documentId: entry.syncUnit.documentId,
+                    scope,
+                    branch: entry.syncUnit.branch,
+                    operations: operations.filter(
+                        op =>
+                            op.index > entry.syncUnit.revision &&
+                            (!since || new Date(op.timestamp) > since)
+                    )
+                });
+            }
+        }
+
+        return strands;
     }
 }
--- src/transmitters/switchboard-push.ts
+++ src/transmitters/switchboard-push.ts
@@ -1,5 +1,54 @@
+import { Operation } from 'document-model/document';
+import {
+    DocumentDriveServer,
+    ListenerRevision,
+    StrandUpdate,
+    UpdateStatus
+} from '..';
+
 export class SwitchboardPushTransmitter {
-    static process() {
-        // push updates to graphql endpoint
+    protected drive: DocumentDriveServer;
+
+    constructor(drive: DocumentDriveServer) {
+        this.drive = drive;
+    }
+
+    async pushStrands(strands: StrandUpdate[]): Promise<ListenerRevision[]> {
+        const results = await Promise.all(
+            strands.map(strand => {
+                const drive = strand.driveId;
+                const documentId = strand.documentId;
+                const scope = strand.scope;
+                const branch = strand.branch;
+                const operations: Operation[] = strand.operations.map(
+                    operation => {
+                        return {
+                            ...operation,
+                            scope,
+                            branch,
+                            index: operation.index,
+                            timestamp: new Date().toTimeString()
+                        };
+                    }
+                );
+
+                return this.drive.addOperations(drive, documentId, operations);
+            })
+        );
+
+        return results.map((result, i) => {
+            const status: UpdateStatus = (
+                result.success ? 'SUCCESS' : 'ERROR'
+            ) as UpdateStatus;
+
+            return {
+                driveId: strands[i]!.driveId,
+                documentId: strands[i]!.documentId,
+                scope: strands[i]!.scope,
+                branch: strands[i]!.branch,
+                status: status,
+                revision: 0
+            };
+        });
     }
 }
