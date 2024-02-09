558c4263cfd0a331cda98a51f0dfc8132f7a7e3a
acaldas <acaldas@powerhouse.inc>
2024-02-09 10:26:17 +0000

    Handle operations with mixed scopes

--- test/operations.test.ts
+++ test/operations.test.ts
@@ -13,10 +13,25 @@ import { DocumentDriveServer } from '../src';
 
 function buildOperation(
     document: DocumentDriveDocument,
-    action: DocumentDriveAction | BaseAction
+    action: DocumentDriveAction | BaseAction,
+    index?: number
 ): Operation<DocumentDriveAction | BaseAction> {
     const newDocument = reducer(document, action);
-    return newDocument.operations[action.scope].slice().pop()!;
+    const operation = newDocument.operations[action.scope].slice().pop()!;
+    return { ...operation, index: index ?? operation.index };
+}
+
+function buildOperations(
+    document: DocumentDriveDocument,
+    actions: Array<DocumentDriveAction | BaseAction>
+): Operation<DocumentDriveAction | BaseAction>[] {
+    const operations: Operation<DocumentDriveAction | BaseAction>[] = [];
+    for (const action of actions) {
+        document = reducer(document, action);
+        const operation = document.operations[action.scope].slice().pop()!;
+        operations.push(operation);
+    }
+    return operations;
 }
 
 describe('Drive operations', () => {
@@ -65,13 +80,14 @@ describe('Drive operations', () => {
             buildOperation(drive, actions.addFolder({ id: '1', name: 'test' }))
         );
 
-        const result = await server.addDriveOperation('1', {
-            ...buildOperation(
+        const result = await server.addDriveOperation(
+            '1',
+            buildOperation(
                 drive,
-                actions.addFolder({ id: '1', name: 'test' })
-            ),
-            index: 1
-        });
+                actions.addFolder({ id: '1', name: 'test' }),
+                1
+            )
+        );
         expect(result.status).toBe('ERROR');
         expect(result.error?.message).toBe('Node with id 1 already exists!');
     });
@@ -92,13 +108,13 @@ describe('Drive operations', () => {
             buildOperation(drive, actions.addFolder({ id: '1', name: 'test' }))
         );
 
-        const result = await server.addDriveOperation('1', {
-            ...buildOperation(
+        const result = await server.addDriveOperation(
+            '1',
+            buildOperation(
                 drive,
                 actions.addFolder({ id: '2', name: 'test 2' })
-            ),
-            index: 0
-        });
+            )
+        );
         expect(result.status).toBe('CONFLICT');
     });
 
@@ -118,13 +134,115 @@ describe('Drive operations', () => {
             buildOperation(drive, actions.addFolder({ id: '1', name: 'test' }))
         );
 
-        const result = await server.addDriveOperation('1', {
-            ...buildOperation(
+        const result = await server.addDriveOperation(
+            '1',
+            buildOperation(
                 drive,
-                actions.addFolder({ id: '2', name: 'test 2' })
-            ),
-            index: 2
-        });
+                actions.addFolder({ id: '2', name: 'test 2' }),
+                2
+            )
+        );
         expect(result.status).toBe('MISSING');
+    });
+
+    it('should accept operations until invalid operation', async () => {
+        await server.addDrive({
+            global: { id: '1', name: 'test', icon: null, slug: null },
+            local: {
+                availableOffline: false,
+                sharingType: 'PRIVATE',
+                listeners: [],
+                triggers: []
+            }
+        });
+        let drive = await server.getDrive('1');
+        const result = await server.addDriveOperations('1', [
+            ...buildOperations(drive, [
+                actions.addFolder({ id: '1', name: 'test 1' }),
+                actions.addFolder({ id: '2', name: 'test 2' }),
+                actions.addFolder({ id: '3', name: 'test 3' })
+            ]),
+            buildOperation(
+                drive,
+                actions.addFolder({ id: '4', name: 'test 4' }),
+                2
+            )
+        ]);
+
+        expect(result.status).toBe('CONFLICT');
+        expect(result.operations.length).toBe(3);
+
+        drive = await server.getDrive('1');
+        expect(drive.state.global.nodes).toStrictEqual([
+            expect.objectContaining({ id: '1', name: 'test 1' }),
+            expect.objectContaining({ id: '2', name: 'test 2' }),
+            expect.objectContaining({ id: '3', name: 'test 3' })
+        ]);
+    });
+
+    it('should apply operations with different scopes', async () => {
+        await server.addDrive({
+            global: { id: '1', name: 'test', icon: null, slug: null },
+            local: {
+                availableOffline: false,
+                sharingType: 'PRIVATE',
+                listeners: [],
+                triggers: []
+            }
+        });
+        let drive = await server.getDrive('1');
+        const result = await server.addDriveOperations(
+            '1',
+            buildOperations(drive, [
+                actions.addFolder({ id: '1', name: 'test 1' }),
+                actions.addFolder({ id: '2', name: 'test 2' }),
+                actions.setAvailableOffline({ availableOffline: true })
+            ])
+        );
+
+        expect(result.status).toBe('SUCCESS');
+        expect(result.operations.length).toBe(3);
+
+        drive = await server.getDrive('1');
+        expect(drive.state.global.nodes).toStrictEqual([
+            expect.objectContaining({ id: '1', name: 'test 1' }),
+            expect.objectContaining({ id: '2', name: 'test 2' })
+        ]);
+        expect(drive.state.local.availableOffline).toBe(true);
+    });
+
+    it('should detect conflict with operations with mixed scopes', async () => {
+        await server.addDrive({
+            global: { id: '1', name: 'test', icon: null, slug: null },
+            local: {
+                availableOffline: false,
+                sharingType: 'PRIVATE',
+                listeners: [],
+                triggers: []
+            }
+        });
+        let drive = await server.getDrive('1');
+        const result = await server.addDriveOperations('1', [
+            ...buildOperations(drive, [
+                actions.addFolder({ id: '1', name: 'test 1' }),
+                actions.addFolder({ id: '2', name: 'test 2' }),
+                actions.setAvailableOffline({ availableOffline: true })
+            ]),
+            buildOperation(
+                drive,
+                actions.addFolder({ id: '3', name: 'test 3' }),
+                1
+            )
+        ]);
+
+        expect(result.status).toBe('CONFLICT');
+        expect(result.operations.length).toBe(3);
+
+        drive = await server.getDrive('1');
+        expect(drive.state.global.nodes).toStrictEqual([
+            expect.objectContaining({ id: '1', name: 'test 1' }),
+            expect.objectContaining({ id: '2', name: 'test 2' })
+        ]);
+        expect(drive.state.local.availableOffline).toBe(true);
     });
 });
