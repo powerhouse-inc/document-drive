630116b4948bce871476397498e9307c9bc62fbc
Frank <frank.pfeift@protonmail.com>
2024-02-27 19:33:39 +0100

    chore: 4 spaces

--- src/server/listener/transmitter/internal.ts
+++ src/server/listener/transmitter/internal.ts
@@ -1,33 +1,33 @@
 import {
-  BaseDocumentDriveServer,
-  Listener,
-  ListenerRevision,
-  StrandUpdate
+    BaseDocumentDriveServer,
+    Listener,
+    ListenerRevision,
+    StrandUpdate
 } from '../../types';
 import { ITransmitter } from './types';
 
 export interface IReceiver {
-  transmit: (strands: StrandUpdate[]) => Promise<ListenerRevision[]>;
+    transmit: (strands: StrandUpdate[]) => Promise<ListenerRevision[]>;
 }
 
 export class InternalTransmitter implements ITransmitter {
-  private drive: BaseDocumentDriveServer;
-  private listener: Listener;
-  private receiver: IReceiver | undefined;
+    private drive: BaseDocumentDriveServer;
+    private listener: Listener;
+    private receiver: IReceiver | undefined;
 
-  constructor(listener: Listener, drive: BaseDocumentDriveServer) {
-    this.listener = listener;
-    this.drive = drive;
-  }
-
-  async transmit(strands: StrandUpdate[]): Promise<ListenerRevision[]> {
-    if (!this.receiver) {
-      return [];
+    constructor(listener: Listener, drive: BaseDocumentDriveServer) {
+        this.listener = listener;
+        this.drive = drive;
     }
-    return this.receiver.transmit(strands);
-  }
 
-  setReceiver(receiver: IReceiver) {
-    this.receiver = receiver
-  }
+    async transmit(strands: StrandUpdate[]): Promise<ListenerRevision[]> {
+        if (!this.receiver) {
+            return [];
+        }
+        return this.receiver.transmit(strands);
+    }
+
+    setReceiver(receiver: IReceiver) {
+        this.receiver = receiver
+    }
 }
