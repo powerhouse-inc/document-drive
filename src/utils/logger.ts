827af5a1036b279dc17a9eaa82d788c9a6992ecc
Frank <frank.pfeift@protonmail.com>
2024-04-10 16:33:22 +0200

    fix: remove sentry

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,18 +1,8 @@
 import winston from 'winston';
-import Sentry from 'winston-sentry-log';
 
 export const logger = winston.createLogger({
     level: 'info',
+    defaultMeta: { service: "document-drive" },
     // format: winston.format.colorize(),
     transports: [new winston.transports.Console()],
 });
-
-if (process.env.NODE_ENV === 'production' && process.env.SENTRY_DSN) {
-    const options = {
-        config: {
-            dsn: process.env.SENTRY_DSN,
-        },
-        level: 'info',
-    };
-    logger.add(new Sentry(options));
-}
