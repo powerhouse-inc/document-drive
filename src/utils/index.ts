d37f6973591e4fa3ecf2d54397bb4559002aac92
Frank <frank.pfeift@protonmail.com>
2024-04-30 11:47:37 +0200

    chore: added helper to identify uuids

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -98,3 +98,7 @@ export function isNoopUpdate(
 export function isBefore(dateA: Date | string, dateB: Date | string) {
     return new Date(dateA) < new Date(dateB);
 }
+
+export function isUUID(value: string) {
+    return /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(value)
+}
