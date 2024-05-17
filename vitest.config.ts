5201282944d8cd1fea6ab25073feccaeeea63b06
Frank <frank.pfeift@protonmail.com>
2024-05-17 15:38:43 +0200

    chore: reduced test timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
     test: {
-        testTimeout: 100000,
+        testTimeout: 5000,
         server: {
             deps: {
                 inline: ['document-model-libs']
