69b1826eaeda188de883f08ee6fb2cbc919fc3a8
Guillermo Puente Sandoval <guillermo.ps09@gmail.com>
2024-05-21 12:30:31 -0400

    feat: fix skip hash validation (#164)
    * feat: fix skip hash validation
    
    * feat: added updated document-model-libs ver
    
    ---------
    
    Co-authored-by: frankp.eth <froid1911@users.noreply.github.com>

--- test/server/driveOperationsConflictResolution.test.ts
+++ test/server/driveOperationsConflictResolution.test.ts
@@ -408,4 +408,170 @@ describe('Drive Operations', () => {
             }
         ]);
     });
+
+    it('should resolve conflicts without duplicate ids when copy folders', async () => {
+        let idCounter = 0;
+        const generateId = () => {
+            idCounter++;
+            return `${idCounter}`;
+        };
+
+        const initialDriveDocument = await buildDrive();
+        let pushOperationResult: IOperationResult;
+
+        DocumentDrive.utils.createDocument();
+
+        const client1 = new DriveBasicClient(
+            server,
+            driveId,
+            initialDriveDocument,
+            DocumentDrive.reducer
+        );
+
+        const client2 = new DriveBasicClient(
+            server,
+            driveId,
+            initialDriveDocument,
+            DocumentDrive.reducer
+        );
+
+        const idFolder1 = generateId();
+        const idFolder2 = generateId();
+
+        expect(idFolder1).toBe('1');
+        expect(idFolder2).toBe('2');
+
+        // Add folders in client 1 and push to server
+
+        client1.dispatchDriveAction(
+            DocumentDrive.actions.addFolder({ id: idFolder1, name: '1' })
+        );
+        pushOperationResult = await client1.pushOperationsToServer();
+        expect(pushOperationResult.status).toBe('SUCCESS');
+
+        client1.dispatchDriveAction(
+            DocumentDrive.actions.addFolder({
+                id: idFolder2,
+                name: '2',
+                parentFolder: idFolder1
+            })
+        );
+        pushOperationResult = await client1.pushOperationsToServer();
+        expect(pushOperationResult.status).toBe('SUCCESS');
+
+        // Sync client 2 with server
+        await client2.syncDocument();
+
+        // Copy folder 1 to root in client 1 and push to server
+        const copyNodesInput = DocumentDrive.utils.generateNodesCopy(
+            {
+                srcId: idFolder1,
+                targetName: '1',
+                targetParentFolder: undefined
+            },
+            generateId,
+            (client1.getDocument() as DocumentDrive.DocumentDriveDocument).state
+                .global.nodes
+        );
+
+        const copyActions = copyNodesInput.map(copyNodeInput =>
+            DocumentDrive.actions.copyNode(copyNodeInput)
+        );
+
+        for (const copyAction of copyActions) {
+            client1.dispatchDriveAction(copyAction);
+        }
+
+        pushOperationResult = await client1.pushOperationsToServer();
+        expect(pushOperationResult.status).toBe('SUCCESS');
+
+        let drive = await server.getDrive(driveId);
+
+        expect(drive.state.global.nodes.length).toBe(4);
+        expect(drive.state.global.nodes).toMatchObject([
+            { id: '1', name: '1', parentFolder: null },
+            { id: '2', name: '2', parentFolder: '1' },
+            { id: '3', name: '1 (copy) 1', parentFolder: null },
+            { id: '4', name: '2', parentFolder: '3' }
+        ]);
+
+        /* CLIENT 2 */
+
+        // generate copy nodes input for client 2
+        const copyNodesInput2 = DocumentDrive.utils.generateNodesCopy(
+            {
+                srcId: idFolder1,
+                targetName: '1',
+                targetParentFolder: undefined
+            },
+            generateId,
+            (client2.getDocument() as DocumentDrive.DocumentDriveDocument).state
+                .global.nodes
+        );
+
+        const copyNodesInput3 = DocumentDrive.utils.generateNodesCopy(
+            {
+                srcId: idFolder1,
+                targetName: '1',
+                targetParentFolder: undefined
+            },
+            generateId,
+            (client2.getDocument() as DocumentDrive.DocumentDriveDocument).state
+                .global.nodes
+        );
+
+        const copyActions2 = copyNodesInput2.map(copyNodeInput =>
+            DocumentDrive.actions.copyNode(copyNodeInput)
+        );
+
+        const copyActions3 = copyNodesInput3.map(copyNodeInput =>
+            DocumentDrive.actions.copyNode(copyNodeInput)
+        );
+
+        // apply copy actions (1) to client 2
+        for (const copyAction of copyActions2) {
+            client2.dispatchDriveAction(copyAction);
+        }
+
+        // push operations to server
+        pushOperationResult = await client2.pushOperationsToServer();
+        expect(pushOperationResult.status).toBe('SUCCESS');
+
+        drive = await server.getDrive(driveId);
+
+        expect(drive.state.global.nodes.length).toBe(6);
+        expect(drive.state.global.nodes).toMatchObject([
+            { id: '1', name: '1', parentFolder: null },
+            { id: '2', name: '2', parentFolder: '1' },
+            { id: '3', name: '1 (copy) 1', parentFolder: null },
+            { id: '4', name: '2', parentFolder: '3' },
+            { id: '5', name: '1 (copy) 2', parentFolder: null },
+            { id: '6', name: '2', parentFolder: '5' }
+        ]);
+
+        // apply copy actions (2) to client 2
+        for (const copyAction of copyActions3) {
+            client2.dispatchDriveAction(copyAction);
+        }
+
+        // sync client 2 with server
+        await client2.syncDocument();
+
+        const client2Nodes = (
+            client2.getDocument() as DocumentDrive.DocumentDriveDocument
+        ).state.global.nodes;
+
+        // TODO: validate that there are not duplicated operations after operation id implementation
+        expect(client2Nodes).toHaveLength(8);
+        expect(client2Nodes).toMatchObject([
+            { id: '1', name: '1', parentFolder: null },
+            { id: '2', name: '2', parentFolder: '1' },
+            { id: '5', name: '1 (copy) 1', parentFolder: null },
+            { id: '6', name: '2', parentFolder: '5' },
+            { id: '3', name: '1 (copy) 2', parentFolder: null },
+            { id: '4', name: '2', parentFolder: '3' },
+            { id: '7', name: '1 (copy) 3', parentFolder: null },
+            { id: '8', name: '2', parentFolder: '7' }
+        ]);
+    });
 });
