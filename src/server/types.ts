ae2fa4438835df3b016b0d981527cd1cee2cd957
Frank <frank.pfeift@protonmail.com>
2024-06-05 13:36:18 +0200

    chore: added queueAction(s) to basedocumentdriveserver

--- src/server/types.ts
+++ src/server/types.ts
@@ -174,6 +174,7 @@ export abstract class BaseDocumentDriveServer {
         operation: Operation,
         forceSync?: boolean
     ): Promise<IOperationResult>;
+
     abstract addOperations(
         drive: string,
         id: string,
@@ -187,10 +188,25 @@ export abstract class BaseDocumentDriveServer {
         operation: Operation,
         forceSync?: boolean
     ): Promise<IOperationResult>;
+
     abstract queueOperations(
         drive: string,
         id: string,
         operations: Operation[],
+        forceSync?: boolean
+    ): Promise<IOperationResult>;
+
+    abstract queueAction(
+        drive: string,
+        id: string,
+        action: Action,
+        forceSync?: boolean
+    ): Promise<IOperationResult>;
+
+    abstract queueActions(
+        drive: string,
+        id: string,
+        actions: Action[],
         forceSync?: boolean
     ): Promise<IOperationResult>;
 
@@ -213,6 +229,18 @@ export abstract class BaseDocumentDriveServer {
     abstract queueDriveOperations(
         drive: string,
         operations: Operation<DocumentDriveAction | BaseAction>[],
+        forceSync?: boolean
+    ): Promise<IOperationResult<DocumentDriveDocument>>;
+
+    abstract queueDriveAction(
+        drive: string,
+        action: DocumentDriveAction | BaseAction,
+        forceSync?: boolean
+    ): Promise<IOperationResult<DocumentDriveDocument>>;
+
+    abstract queueDriveActions(
+        drive: string,
+        actions: Array<DocumentDriveAction | BaseAction>,
         forceSync?: boolean
     ): Promise<IOperationResult<DocumentDriveDocument>>;
 
