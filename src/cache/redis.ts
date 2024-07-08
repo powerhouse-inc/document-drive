c94030bcb79248aee9bd5c96af7745e9165fbd5d
Frank <frank.pfeift@protonmail.com>
2024-07-08 10:32:40 +0200

    chore: updated ids of cache and queue

--- src/cache/redis.ts
+++ src/cache/redis.ts
@@ -12,7 +12,7 @@ class RedisCache implements ICache {
     }
 
     private static _getId(drive: string, id: string) {
-        return `${drive}:${id}`;
+        return `cache:${drive}:${id}`;
     }
 
     async setDocument(drive: string, id: string, document: Document) {
@@ -38,13 +38,15 @@ class RedisCache implements ICache {
     }
 
     async getDocument(drive: string, id: string) {
-        const doc = await this.redis.hGet(drive, id);
+        const redisId = RedisCache._getId(drive, id);
+        const doc = await this.redis.get(redisId);
 
         return doc ? JSON.parse(doc) as Document : undefined;
     }
 
     async deleteDocument(drive: string, id: string) {
-        return (await this.redis.hDel(drive, id)) > 0;
+        const redisId = RedisCache._getId(drive, id);
+        return (await this.redis.del(redisId)) > 0;
     }
 }
 
